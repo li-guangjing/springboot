75136d1cfec04e7e26bbfe13b9d79dfc46a89e2d
Benjamin-GitHub <648822980@qq.com>
2020-11-07 23:52:36 +0800

    test启动成功版

--- src/test/java/com/li/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/li/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,25 @@
 package com.li.springboot;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
+    @BeforeEach
+    void before(){
+        System.out.println("this is before test message");
+    }
 	@Test
 	void contextLoads() {
+		System.out.println("this is test message");
 	}
+
+	@AfterEach
+    void after(){
+        System.out.println("this is after test message");
+    }
 
 }
